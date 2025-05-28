77b7b752ea52e050b594d0159e42233f1df99b48
Hrishabh Singh <rishab18011@gmail.com>
2025-05-28 17:29:28 +0530

    chore: initial project setup with base structure

---
+++ README.md
@@ -0,0 +1,1 @@
+# <img src="./public/paperloom.png" alt="paperloom Logo" class="w-10 h-10 pr-2 inline-block" style="height: 86px; vertical-align: middle;" />
---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+
+import "@/app/globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  variable: "--font-poppins",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata: Metadata = {
+  title: "paperloom - effortless pdf sharing and collaboration",
+  description:
+    "paperloom is a platform that simplifies PDF collaboration—enabling seamless sharing, feedback, and teamwork.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${poppins.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
